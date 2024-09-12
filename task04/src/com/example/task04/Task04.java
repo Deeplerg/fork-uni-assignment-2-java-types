df2fe549c00cffe250759eb47c985577bfaa9f96
Deeplerg <deeplerg@gmail.com>
2024-09-12 20:16:04 +0500

    Complete task 4

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -2,11 +2,20 @@ package com.example.task04;
 
 public class Task04 {
 
-    public static float calculate(int a, int b, String operation) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+    public static float calculate(int a, int b, String operation) throws UnsupportedOperationException {
+        switch (operation) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return (float)a / b;
+            default:
+                // maybe IllegalArgumentException would be more appropriate?
+                throw new UnsupportedOperationException(operation + " is not a valid operation.");
+        }
     }
 
     public static void main(String[] args) {
